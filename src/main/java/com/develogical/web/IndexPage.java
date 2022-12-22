79a8111c7d01fcaf791fbbfc4fc5a1785ad2370d
valen <meowww717@gmail.com>
2022-12-22 14:27:42 +0000

    added one more header

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome Developer!</h1>" +
+                "<h1>Welcome Developer!</h1>" + "<h1>Have a good day!</h1>" +
                 "<p>Please enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
